ad30c46aceace7f018754a54778d10dd8292dda8
Cole <oftedahl.cole@gmail.com>
2024-11-08 15:01:10 -0600

    Added faction selector component

--- 40K_Roster_Builder/src/components/MainContentPage/FactionSelector/FactionSelector.tsx
+++ 40K_Roster_Builder/src/components/MainContentPage/FactionSelector/FactionSelector.tsx
@@ -1,3 +1,7 @@
+import { Autocomplete, Box, TextField } from "@mui/material";
+import { useState } from "react";
+import { FactionList } from "../../UtilityComponents/Army_Constants/Army_Constants";
+import './FactionSelector.css';
 
 interface FactionSelectorProps {
 
@@ -5,8 +9,45 @@ interface FactionSelectorProps {
 
 const FactionSelector: React.FC<FactionSelectorProps> = () => {
 
+  const factionOptions = [
+    FactionList.IMPERIUM.name, 
+    FactionList.SPACE_MARINES.name, 
+    FactionList.CHAOS.name, 
+    FactionList.XENOS.name,  
+  ]
+
+  const [armyOptions, setArmyOptions] = useState();
+
+  const [faction, setFaction] = useState<string | null>();
+
+  const handleFactionChange = (e: React.SyntheticEvent, value: string | null) => {
+    setFaction(value);
+    //also need to clear army selector
+  };
+
   return (
     <>
+      <Box className="FactionSelectorBox">
+        <Autocomplete 
+          options={factionOptions} 
+          openOnFocus 
+          className={"FactionSelector_FactionDropdown" + ((faction === undefined || faction === "" || faction === null) ? "" : "_filled")}
+          renderInput={(params) => <TextField {...params} label="Faction"/>} 
+          onChange={handleFactionChange}
+        ></Autocomplete>
+        <Autocomplete 
+          options={factionOptions} 
+          openOnFocus 
+          className={"FactionSelector_ArmyDropdown" + ((faction === undefined || faction === "" || faction === null) ? "_disabled" : "")}
+          renderInput={(params) => <TextField {...params} label="Army"/>}
+        ></Autocomplete>
+        <Autocomplete 
+          options={factionOptions} 
+          openOnFocus 
+          onClick={focus} 
+          renderInput={(params) => <TextField {...params} />}
+        ></Autocomplete>
+      </Box>
     </>
   );
 }
--- 40K_Roster_Builder/src/components/MainContentPage/MainContentPage.tsx
+++ 40K_Roster_Builder/src/components/MainContentPage/MainContentPage.tsx
@@ -3,6 +3,7 @@ import CollapsibleCard from "../UtilityComponents/CollapsibleCard/CollapsibleCar
 import { useState } from "react";
 import './MainContentPage.css';
 import RosterBuildingArea from "./RosterBuildingArea/RosterBuildingArea";
+import FactionSelector from "./FactionSelector/FactionSelector";
 
 interface MainContentPageProps {
 
@@ -22,7 +23,8 @@ const MainContentPage: React.FC<MainContentPageProps> = () => {
     <Box className="MainContentPageOuterContainer">
       <Box className="MainContentPageContainer">
         <CollapsibleCard summary="Faction Selector" expanded={activeCollapsibleBox === CollapsibleBoxNames[0]} onChange={handleBoxClick} boxName={CollapsibleBoxNames[0]}>
-          <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</Typography>
+          {/* <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</Typography> */}
+          <FactionSelector/>
         </CollapsibleCard>
         <CollapsibleCard summary="Roster Builder"  expanded={activeCollapsibleBox === CollapsibleBoxNames[1]} onChange={handleBoxClick} boxName={CollapsibleBoxNames[1]}>
           <RosterBuildingArea factionName={"Tyranids"} detachmentName="Ravener Hive"/>
