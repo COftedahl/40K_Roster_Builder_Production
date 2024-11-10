b8ce8976de3c3f6517baf626d4b61c6dd079c2cf
Cole <oftedahl.cole@gmail.com>
2024-11-10 12:03:04 -0600

    Applied transition to all boxes

--- 40K_Roster_Builder/src/components/MainContentPage/FactionSelector/FactionSelector.tsx
+++ 40K_Roster_Builder/src/components/MainContentPage/FactionSelector/FactionSelector.tsx
@@ -19,9 +19,21 @@ const FactionSelector: React.FC<FactionSelectorProps> = () => {
   const [armyOptions, setArmyOptions] = useState();
 
   const [faction, setFaction] = useState<string | null>();
+  const [army, setArmy] = useState<string | null>();
+  const [detachment, setDetachment] = useState<string | null>();
 
   const handleFactionChange = (e: React.SyntheticEvent, value: string | null) => {
     setFaction(value);
+    //also need to clear army selector
+  };
+  
+  const handleArmyChange = (e: React.SyntheticEvent, value: string | null) => {
+    setArmy(value);
+    //also need to clear army selector
+  };
+
+  const handleDetachmentChange = (e: React.SyntheticEvent, value: string | null) => {
+    setDetachment(value);
     //also need to clear army selector
   };
 
@@ -35,22 +47,23 @@ const FactionSelector: React.FC<FactionSelectorProps> = () => {
           renderInput={(params) => <TextField {...params} label="Faction"/>} 
           onChange={handleFactionChange}
         ></Autocomplete>
-        <br className="FactionSelector_AutocompleteLineBreak"/>
         <Autocomplete 
           options={factionOptions} 
           openOnFocus 
           className={"FactionSelector_ArmyDropdown" + ((faction === undefined || faction === "" || faction === null) ? "_disabled" : "")}
           disabled={(faction === undefined || faction === "" || faction === null) ? true : false}
           renderInput={(params) => <TextField {...params} label="Army"/>}
+          onChange={handleArmyChange}
         ></Autocomplete>
-        <Box>
           <Autocomplete 
             options={factionOptions} 
             openOnFocus 
+            className={"FactionSelector_DetachmentDropdown" + ((army === undefined || army === "" || army === null) ? "_disabled" : "")}
+            disabled={(army === undefined || army === "" || army === null) ? true : false}
             onClick={focus} 
-            renderInput={(params) => <TextField {...params} />}
+            renderInput={(params) => <TextField {...params} label="Detachment"/>}
+            onChange={handleDetachmentChange}
           ></Autocomplete>
-        </Box>
       </Box>
     </>
   );
